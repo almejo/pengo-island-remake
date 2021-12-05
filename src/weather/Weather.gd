ae690fd9fd7ff8f94a6d4445b8cc54b91ae27d41
Alejandro Vera <alejandro.vera@gmail.com>
2021-12-05 16:27:12 -0300

    add fog

--- src/weather/Weather.gd
+++ src/weather/Weather.gd
@@ -3,3 +3,5 @@ extends CanvasLayer
 func _unhandled_input(event: InputEvent):
 	if event.is_action_pressed("toggle_snow"):
 		$Snow.emitting = !$Snow.emitting
+	elif event.is_action_pressed("toggle_fog"):
+		$fog.visible = !$fog.visible
